ecfe7fca5136d590781210a81a02f7a7ec4b9efe
Daniel Morales <daniel.a.moralesalt@gmail.com>
2020-09-17 00:50:06 -0400

    :rocket: Implemented stack with arrays

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,25 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size = len(self.storage)
+        return value
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            value = self.storage.pop()
+            self.size = len(self.storage)
+            return value
